118e7c78d88bcc57f8f72e9fed687f7c2c5a107e
wisekim <lwisekiml@naver.com>
2024-02-19 21:47:28 +0900

    #8 - Spring Transactional 사용시 주의사항
    foo()에만 @Transactional이 있다면 롤백이 안된다.
    bar()에 @Transactional을 넣어줘야 제대로 롤백이 되게 된다.
    (에러 발생시 롤백이 되는 것이 정상이다.)

--- src/main/java/com/example/project/pharmacy/service/PharmacyRepositoryService.java
+++ src/main/java/com/example/project/pharmacy/service/PharmacyRepositoryService.java
@@ -6,7 +6,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -16,6 +18,29 @@ import java.util.Optional;
 public class PharmacyRepositoryService {
 
     private final PharmacyRepository pharmacyRepository;
+
+    // self invocation test
+    @Transactional
+    public void bar(List<Pharmacy> pharmacyList) {
+        log.info("bar CurrentTransactionName: " + TransactionSynchronizationManager.getCurrentTransactionName());
+        foo(pharmacyList);
+    }
+
+    @Transactional
+    public void foo(List<Pharmacy> pharmacyList) {
+        log.info("foo CurrentTransactionName: " + TransactionSynchronizationManager.getCurrentTransactionName());
+        pharmacyList.forEach(pharmacy -> {
+            pharmacyRepository.save(pharmacy);
+            throw new RuntimeException("error"); // 에러가 발생해서 롤백이 되어야 한다.(bar에 있는 @Transactional을 주석처리하면 롤백이 발생하지 않는다.)
+        });
+    }
+
+    // read only test
+    @Transactional(readOnly = true) // 읽기 전용으로 설정하게 되면, JPA에서 스냅샷 저장 및 Dirty Checking 작업을 수행하지 않기 때문에 성능적으로 이점
+    public void startReadOnlyMethod(Long id) {
+        pharmacyRepository.findById(id).ifPresent(pharmacy ->
+                pharmacy.changePharmacyAddress("서울 특별시 광진구"));
+    }
 
     @Transactional
     public void updateAddress(Long id, String address) {
@@ -38,5 +63,10 @@ public class PharmacyRepositoryService {
         }
 
         entity.changePharmacyAddress(address);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Pharmacy> findAll() {
+        return pharmacyRepository.findAll();
     }
 }
