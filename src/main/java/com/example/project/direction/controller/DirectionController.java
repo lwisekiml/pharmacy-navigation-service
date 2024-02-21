48f9744ed585ef416ac28a0f86567ce11d73bd14
wisekim <lwisekiml@naver.com>
2024-02-21 19:35:00 +0900

    #22 - 테스트 코드 작성
    - encode, decode 테스트
    - redirection 테스트

--- src/main/java/com/example/project/direction/controller/DirectionController.java
+++ src/main/java/com/example/project/direction/controller/DirectionController.java
@@ -1,13 +1,11 @@
 package com.example.project.direction.controller;
 
-import com.example.project.direction.entity.Direction;
 import com.example.project.direction.service.DirectionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @Controller
 @Slf4j
@@ -18,15 +16,11 @@ public class DirectionController {
     private static final String DIRECTION_BASE_URL = "https://map.kakao.com/link/map/";
 
     @GetMapping("/dir/{encodedId}")
-    public String searchDirection(@PathVariable("encoded") String encodedId) {
-        Direction resultDirection = directionService.findById(encodedId);
+    public String searchDirection(@PathVariable("encodedId") String encodedId) {
 
-        String params = String.join(",", resultDirection.getTargetPharmacyName(),
-                String.valueOf(resultDirection.getTargetLatitude()), String.valueOf(resultDirection.getTargetLongitude()));
+        String result = directionService.findDirectionUrlById(encodedId);
 
-        String result = UriComponentsBuilder.fromHttpUrl(DIRECTION_BASE_URL + params)
-                .toUriString();
-        log.info("direction params: {}, url: {}" ,params ,result);
+        log.info("[DirectionController searchDirection] direction url: {}", result);
 
         return "redirect:" + result;
     }
