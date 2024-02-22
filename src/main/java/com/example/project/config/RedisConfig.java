a915fec53c22726f96420acc96cb709fd7612927
wisekim <lwisekiml@naver.com>
2024-02-22 16:38:47 +0900

    #23 - Redis 자료구조를 이용하여 구현

---
+++ src/main/java/com/example/project/config/RedisConfig.java
@@ -0,0 +1,34 @@
+package com.example.project.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Bean // 커넥션 정보를 빈으로 등록
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisHost, redisPort); // Lettuce 사용권장
+    }
+
+    @Bean // RedisTemplate을 빈으로 등록하고 , 이 RedisTemplate을 주입받아 사용하고자 하는 자료구조를 사용할 것이다.
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+}
